8298c50b5b9f03b03fa9e9ce08535a4de924c58f
my-first-repo <saiyadul77@gmail.com>
2022-04-17 21:15:39 +0600

    bananer updated

---
+++ src/Pages/Home/Banner/Banner.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import { Carousel } from 'react-bootstrap';
+import science1 from '../../../images/banner/science1.jpg';
+import science2 from '../../../images/banner/science2.jpg';
+import science3 from '../../../images/banner/science3.jpg';
+
+const Banner = () => {
+    return (
+        <div>
+            <Carousel>
+                <Carousel.Item>
+                    <img
+                        className="d-block w-100 fluid"
+                        src={science1}
+                        alt="First slide"
+                    />
+                    <Carousel.Caption>
+                        <h3>First slide label</h3>
+                        <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                    </Carousel.Caption>
+                </Carousel.Item>
+                <Carousel.Item>
+                    <img
+                        className="d-block w-100 fluid"
+                        src={science2}
+                        alt="Second slide"
+                    />
+
+                    <Carousel.Caption>
+                        <h3>Second slide label</h3>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                    </Carousel.Caption>
+                </Carousel.Item>
+                <Carousel.Item>
+                    <img
+                        className="d-block w-100 fluid"
+                        src={science3}
+                        alt="Second slide"
+                    />
+
+                    <Carousel.Caption>
+                        <h3>Third slide label</h3>
+                        <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+                    </Carousel.Caption>
+                </Carousel.Item>
+            </Carousel>
+        </div>
+    );
+};
+
+export default Banner;
