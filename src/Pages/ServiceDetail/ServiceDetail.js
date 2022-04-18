770276ac7f1d09ec3716e1676eaa96ce1be45a67
my-first-repo <saiyadul77@gmail.com>
2022-04-18 23:54:03 +0600

    updated

--- src/Pages/ServiceDetail/ServiceDetail.js
+++ src/Pages/ServiceDetail/ServiceDetail.js
@@ -6,6 +6,12 @@ const ServiceDetail = () => {
     return (
         <div className='text-center'>
             <h1>This is service detail:{serviceId}</h1>
+            <h2>Common Features:</h2>
+            <p>Physical or online class</p>
+            <p>One to one or grouping class</p>
+            <p>Special care for weak student</p>
+            <p>Always give motivation speech</p>
+
             <div>
                 <Link to='/checkout'>
                     <button className='btn btn-primary'>Proceed Checkout</button>
